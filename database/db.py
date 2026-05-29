cfa3930d2294b8fc7c62270b166fd04edbcd9321
Mantavya <themantavyasethi@gmail.com>
2026-05-29 15:51:05 -0400

    added logic to create tables in the postgresql docker container

--- database/db.py
+++ database/db.py
@@ -15,11 +15,51 @@ try:
 
     cur = conn.cursor()
 
-    cur.execute("SELECT current_database();")
+    cur.execute("""SELECT current_database();""")
 
     print("Connected successfully!")
     print("Database:", cur.fetchone()[0])
 
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS rolling_standings (
+        season INTEGER,
+        matchday INTEGER,
+        position INTEGER,
+        team_id INTEGER,
+        team_name TEXT,
+        short_name TEXT,
+        tla VARCHAR(3),
+        crest_link TEXT,
+        played INTEGER,
+        won INTEGER,
+        draw INTEGER,
+        loss INTEGER,
+        points INTEGER,
+        goals_for INTEGER,
+        goals_against INTEGER,
+        goal_difference INTEGER,
+        PRIMARY KEY (season, matchday, team_id)
+        );
+    """)
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS topscorers (
+    season INTEGER,
+    matchday INTEGER,
+    player_id INTEGER,
+    player_name TEXT,
+    nationality TEXT,
+    team_id INTEGER,
+    team_name TEXT,
+    matches_played INTEGER,
+    goals_scored INTEGER,
+    assists INTEGER,
+    penalties INTEGER,
+    PRIMARY KEY (season, matchday, player_id)
+    )
+    """)
+
+    conn.commit()
     cur.close()
     conn.close()
 
