ce256768f438f7ed91d33aebb899d0a00186ddca
Mantavya <themantavyasethi@gmail.com>
2026-06-14 17:21:49 -0400

    final commit

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -60,7 +60,7 @@ resource "aws_security_group_rule" "ssh_ingress" {
   to_port           = 22
   protocol          = "tcp"
   security_group_id = aws_security_group.prem_analytics_sg.id
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = ["142.189.201.143/32"]
 }
 
 resource "aws_security_group_rule" "flask_ingress" {
